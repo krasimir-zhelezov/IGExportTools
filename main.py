f3ef1224b5fbf50993918b789706e9df379b2af0
krasimir-zhelezov <krasizh07@gmail.com>
2025-04-26 19:39:35 +0300

    chore: beautified output

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import csv
 import pandas as pd
-
-df = pd.read_csv("followers.csv")
 
 username = input('Instagram username: @')
 
 followers = set(user for user in pd.read_csv(f"IGExport_Followers_{username}.csv")["userName"])
 following = set(user for user in pd.read_csv(f"IGExport_Following_{username}.csv")["userName"])
+
+print('Not following me back: ')
 print(following - followers) #* not following me back
-# print(followers - following) #* i don't follow back
+print('-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+
+print('I\'m not following back: ')
+print(followers - following) #* i don't follow back
